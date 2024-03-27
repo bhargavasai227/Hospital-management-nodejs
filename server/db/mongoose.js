19319bbfc647fdd471abe083f8ef1b56b49fa10b
Bhargav Dasari <112853876+bhargavasai227@users.noreply.github.com>
2024-03-27 18:42:08 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb+srv://bhargav:bhargav227@cluster0.r0cqany.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 module.exports = {mongoose};
